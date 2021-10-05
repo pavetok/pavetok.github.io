f2edeace9e05301d3f612b4f8c196f8784fc716b
Pavel Vetokhin <p.vetokhin@gmail.com>
2021-10-05 20:55:50 +0300

    minify and inline css

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,22 @@
+const CleanCSS = require("clean-css");
+
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = eleventyConfig => {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addFilter("minify", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-      layouts: "_layouts",
-    },
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
-  };
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+            layouts: "_layouts",
+        },
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        htmlTemplateEngine: "njk"
+    };
 };
